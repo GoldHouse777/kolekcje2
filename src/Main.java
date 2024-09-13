5da0c01311c0816b5f544e80ee0121f5f9e6f567
GoldHouse777 <psoltysik01@gmail.com>
2024-09-13 11:37:43 +0200

    notatka do kartkowki

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,35 @@ public class Main {
     public static void main(String[] args) {
         Gra gra1 = new Gra();
         gra1.zagraj();
-
+        System.out.println("pierwsze" + sitoEra(200));
     }
 
+    //do kartkowki
+    static ArrayList<Integer> sitoEra(int liczba) {
+        //przygotowanie listy z warosciami logicznymi
+        ArrayList<Boolean> logiczne = new ArrayList<>();
+        logiczne.add(false);
+        logiczne.add(false);
+        for (int i = 2; i < liczba; i++) {
+            logiczne.add(true);
+        }
+        //wykreslanie wieloktornosci liczb pierwszych
+        for (int i = 0; i < liczba; i++) {
+            if (logiczne.get(i).equals(true)) {
+                //wykreslanie zmiana na false
+                for (int k = i * 2; k < liczba; k = k + i) {
+                    logiczne.set(k, false);
+
+                }
+            }
+        }
+        ArrayList<Integer> pierwsze = new ArrayList<>();
+        for (int i = 0; i < logiczne.size(); i++) {
+            if (logiczne.get(i).equals(true)) {
+                pierwsze.add(i);
+            }
+
+        }
+        return pierwsze;
+    }
 }
